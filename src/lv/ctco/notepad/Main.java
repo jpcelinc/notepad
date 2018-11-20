7a2c8dcd6abab6d4bca3e9b8722ba990be1e1dc1
jpcelinc <44901033+jpcelinc@users.noreply.github.com>
2018-11-20 13:15:53 +0200

    Update Main.java

--- src/lv/ctco/notepad/Main.java
+++ src/lv/ctco/notepad/Main.java
@@ -4,32 +4,53 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-   static Scanner scanner = new Scanner(System.in);
-   static List<Person> records = new ArrayList<Person>() ;
+    static Scanner scanner = new Scanner(System.in);
+    static List<Person> persons = new ArrayList<>();
 
     public static void main(String[] args) {
-        for(;;){
-         System.out.print("cmd:");
-         String cmd  = scanner.next();
-         switch (cmd) {
-		 case :create":
-			 createPerson();
-			 break;
-		 case "help":
-             showHelp();
-             break;
-		 case "list":
-                 list();
-                 break;
-		 case "exit":
-                return;
-                 break;	 
-		 default:
-			 System.out.println("Wrong command. Try 'help'");
+        for (; ; ) {
+            System.out.print("cmd:");
+            String cmd = scanner.next();
+            switch (cmd) {
+         case :create ":
+                createPerson();
+                break;
+                case "help":
+                    showHelp();
+                    break;
+                case "list":
+                    showList();
+                    break;
+                case "exit":
+                    return;
+                break;
+                default:
+                    System.out.println("Wrong command. Try 'help'");
 
-         }
+            }
 
         }
-	// write your code here
+
+    }
+    private static void showList() {
+        for (Person person : persons) {
+         System.out.println(  person.firstName + "; " + person.lastName ) ;
+
+    }
+    private static void showHelp() {
+        System.out.println("Please enter first name and last name");
+
+    }
+    private static void createPerson() {
+        System.out.println("What is your first name?");
+        String name1 = scan.next();
+        System.out.println("What is your last name?");
+        String name2 = scan.next();
+        Person person = new Person();
+        person.firstName = name1;
+        person.lastName = name2;
+        persons.add(person);
+
+
     }
 }
