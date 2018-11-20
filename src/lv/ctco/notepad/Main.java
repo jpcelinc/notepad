af54788f6b03c891335bbab3ced10e7778aca958
yelena.pchelinceva <pcelena@inbox..lv>
2018-11-20 17:05:06 +0200

    commit 20.11

--- src/lv/ctco/notepad/Main.java
+++ src/lv/ctco/notepad/Main.java
@@ -1,20 +1,21 @@
 package lv.ctco.notepad;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
     static List<Person> persons = new ArrayList<>();
-
     public static void main(String[] args) {
         for (; ; ) {
+       //     String askString("cmd:");
             System.out.print("cmd:");
             String cmd = scanner.next();
             switch (cmd) {
-         case :create ":
-                createPerson();
-                break;
+                case "create":
+                    createPerson();
+                    break;
                 case "help":
                     showHelp();
                     break;
@@ -23,7 +24,6 @@ public class Main {
                     break;
                 case "exit":
                     return;
-                break;
                 default:
                     System.out.println("Wrong command. Try 'help'");
 
@@ -32,25 +32,59 @@ public class Main {
         }
 
     }
-    private static void showList() {
-        for (Person person : persons) {
-         System.out.println(  person.firstName + "; " + person.lastName ) ;
 
+    private static void showList() {
+
+        persons.forEach(person -> System.out.println(person));
+        //   for (Person person : persons) {
+        //       System.out.println(person.firstName + "; " + person.lastName);
+        //   }
     }
+
     private static void showHelp() {
         System.out.println("Please enter first name and last name");
 
     }
+
     private static void createPerson() {
-        System.out.println("What is your first name?");
-        String name1 = scan.next();
-        System.out.println("What is your last name?");
-        String name2 = scan.next();
+
         Person person = new Person();
-        person.firstName = name1;
-        person.lastName = name2;
+       // System.out.println("What is your first name?");
+        person.setFirstName(scanner.next());
+       // System.out.println("What is your last name?");
+        person.setLastName(scanner.next());
+      //  System.out.println("What is your email?");
+        person.setEmail(scanner.next());
         persons.add(person);
 
 
+    }
+
+    private String askString (String msg) {
+        List<String>  inputList = new ArrayList<>();
+        String rString = "";
+
+        do while (true) {
+            System.out.println(msg);
+            rString = scanner.next();
+             if (rString.startsWith("\"")) {
+                inputList.add(rString);
+               return true ;
+            }
+
+              return  false;
+            }
+
+        }
+        return
+    }
+    private static boolean checkInput(String input) {
+        String[] inputOptions = {"create", "help","list","exit"};
+        for (String i : inputOptions) {
+            if (input.equals(i)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/lv/ctco/notepad/Person.java
+++ src/lv/ctco/notepad/Person.java
@@ -4,6 +4,52 @@ package lv.ctco.notepad;
  * Created by yelena.pchelinceva on 11/16/2018.
  */
 public class Person {
-    String firstName;
-    String lastName;
-   }
+    private static int counter = 0;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+ public Person() {
+     counter++;
+     id = counter;
+ }
+    public int getId() {
+        return id;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", counter='" + counter + '\'' +
+                '}';
+    }
+}
